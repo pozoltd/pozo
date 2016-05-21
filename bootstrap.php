5a44e3d2f824d865fb7d96cabe32bea85fff6132
Weida <pozoltd@gmail.com>
2016-05-22 11:13:45 +1200

    try again

--- bootstrap.php
+++ bootstrap.php
@@ -36,7 +36,7 @@ $app['swiftmailer.options'] = array(
     'port' => SMTP_PORT,
     'username' => SMTP_USER,
     'password' => SMTP_PASS,
-    'encryption' => false
+    'encryption' => 'STARTTLS'
 );
 
 $app->register(new Silex\Provider\ValidatorServiceProvider(), array());
